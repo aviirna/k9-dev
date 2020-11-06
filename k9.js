5fb32b83d2b279f3cd05a85654734d93f5eba76b
JoshuaMEllis <joshuamellis5@gmail.com>
2020-11-06 16:45:20 +0000

    abacus

--- k9.js
+++ k9.js
@@ -1,17 +1,18 @@
 const Kiss = require("kiss-ssg");
 const kiss = new Kiss();
 
-kiss.pages(
-  {
-    view: "course.hbs",
-    model: "courses",
-    nav: true,
-  },
-  ({ model }) => {
-    return {
-      slug: model.slug,
-      title: model.title,
-      model: model,
-    };
-  }
-);
+kiss
+  .pages(
+    {
+      view: "course.hbs",
+      model: "courses",
+      nav: true,
+    },
+    ({ model }) => {
+      return {
+        slug: model.slug,
+        title: model.title,
+        model: model,
+      };
+    }
+  );
